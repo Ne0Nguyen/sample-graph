efc5c33e2e7f7a7f10860441c3dce844ddcb2598
whatever071917 <whatever071197@gmail.com>
2025-03-07 19:24:53 +0700

    change lib

--- sample-graph/src/app/app.component.ts
+++ sample-graph/src/app/app.component.ts
@@ -1,20 +1,23 @@
 import {Component, OnInit} from '@angular/core';
-import {Color, NgxChartsModule, ScaleType} from "@swimlane/ngx-charts";
+import {Color, LegendPosition, NgxChartsModule, ScaleType} from "@swimlane/ngx-charts";
 import {data} from "./data";
-import {NgxEchartsModule} from "ngx-echarts";
+import {NgApexchartsModule} from "ng-apexcharts";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgxChartsModule, NgxEchartsModule],
+  imports: [NgxChartsModule, NgApexchartsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent implements OnInit {
   title = 'sample-graph';
   data = data
   graphData: any;
-  currentValue:number = 0
+  currentValue: number = 0
+  chartOptions: any = {};
+
 
   colorScheme: Color = {
     name: 'color',
@@ -22,6 +25,7 @@ export class AppComponent implements OnInit {
     group: ScaleType.Time,
     domain: ['#28a745', '#dc3545', '#6c757d']
   };
+  protected readonly LegendPosition = LegendPosition;
 
   ngOnInit() {
     this.graphData = this.data.map(item => {
@@ -31,10 +35,66 @@ export class AppComponent implements OnInit {
       }
     })
     this.currentValue = this.graphData[0].value;
+
+    this.chartOptions = {
+      series: this.graphData.map((item: any) => item.value),
+      chart: {
+        height: 350,
+        type: "radialBar"
+      },
+      fill: {
+        type: "solid",
+        color: "#00E396"
+      },
+      plotOptions: {
+        radialBar: {
+          startAngle: -100,
+          endAngle: 100,
+          hollow: {
+            margin: 0,
+            size: "40%",
+            background: "white"
+          },
+          dataLabels: {
+            name: {
+              show: true,
+              fontSize: "22px",
+            },
+            value: {
+              show: true,
+              fontSize: "16px",
+            },
+            total: {
+              show: true,
+              label: "Clinicals",
+              formatter: function (w: any) {
+                return "100%";
+              }
+            },
+            track: {
+              strokeWidth: "100%",
+              background: "#E8E8E8",
+            },
+          }
+        }
+      },
+      labels: this.graphData.map((item: any) => item.name),
+      legends: {
+        show: true,
+        position: 'bottom',
+      },
+    };
+    console.log(this.chartOptions);
   }
 
   onSelect(item: any) {
+    console.log(item)
     this.currentValue = item.value;
   }
 
+  getLabelColor = () =>{
+    return this.graphData.map((item: any) =>
+      item.value === 0 ? "#B0B0B0" : item.value > 70 ? "#00E396" : "#FF4560"
+    )
+  }
 }
